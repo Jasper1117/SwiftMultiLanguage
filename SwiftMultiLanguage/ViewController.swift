dd9a4cf0e053456935613826188c1f7e466a53de
Jasper <jasperxu1117@gmail.com>
2025-02-06 15:33:17 +0800

    Initial Commit

---
+++ SwiftMultiLanguage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftMultiLanguage
+//
+//  Created by Jasper Hui on 6/2/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SwiftMultiLanguageTests/SwiftMultiLanguageTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftMultiLanguageTests.swift
+//  SwiftMultiLanguageTests
+//
+//  Created by Jasper Hui on 6/2/2025.
+//
+
+import Testing
+@testable import SwiftMultiLanguage
+
+struct SwiftMultiLanguageTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
