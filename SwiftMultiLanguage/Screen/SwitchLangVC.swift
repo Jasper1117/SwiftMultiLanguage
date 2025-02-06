ce509e8a28e2b2e40c8dc181416f1469a6ef08b5
Jasper <jasperxu1117@gmail.com>
2025-02-06 19:52:58 +0800

    localization

---
+++ SwiftMultiLanguage/Screen/SwitchLangVC.swift
@@ -0,0 +1,29 @@
+//
+//  SwitchLangVC.swift
+//  SwiftMultiLanguage
+//
+//  Created by Jasper Hui on 6/2/2025.
+//
+
+import UIKit
+
+class SwitchLangVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ SwiftMultiLanguage/UI/MLLabel.swift
@@ -0,0 +1,20 @@
+//
+//  MLLabel.swift
+//  SwiftMultiLanguage
+//
+//  Created by Jasper Hui on 6/2/2025.
+//
+
+import UIKit
+
+class MLLabel: UILabel {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
